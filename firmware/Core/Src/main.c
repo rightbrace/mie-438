f48bf06bc8b5189a44a3e4249600a5d94d69bbfa
Josef Duchesne <josefduchesne@outlook.com>
2024-03-25 11:01:07 -0400

    initial

---
+++ firmware/Core/Src/main.c
@@ -0,0 +1,661 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_TIM3_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+typedef struct {
+	uint32_t  red;
+	uint32_t  green;
+	uint32_t  blue;
+	uint8_t*  data;
+} Reading;
+
+
+Reading KnownValues[] = {
+		{2600, 2800, 2750, "Nothing"},
+		{1000, 2700, 2765, "Red"},
+		{800, 1750, 2450, "Yellow"},
+		{2100, 2100, 2300, "Green"},
+		{2100, 2300, 2100, "Blue"},
+};
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) > (b) ? (a) : (b))
+
+uint32_t CalculateDistance(Reading a, Reading b) {
+	// You would think this would be an issue with unsigned values, but it just works (tm)
+	return (a.red - b.red) * (a.red - b.red) + (a.green - b.green) * (a.green - b.green) + (a.blue - b.blue) * (a.blue - b.blue);
+}
+
+void SetColor(bool red, bool green, bool blue) {
+
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, !blue);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, !red);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, !green);
+
+}
+
+uint32_t ReadAnalog() {
+	uint32_t result = 0;
+	HAL_ADC_Start(&hadc1);
+	if (HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK) {
+		result = HAL_ADC_GetValue(&hadc1);
+	}
+	HAL_ADC_Stop(&hadc1);
+
+	return result;
+}
+
+void CaptureReading(Reading* result) {
+
+	const int CHARGE_DELAY = 150;
+	const int DISCHARGE_DELAY = 150;
+
+	SetColor(1, 0, 0);
+	HAL_Delay(CHARGE_DELAY);
+	result->red = ReadAnalog();
+	SetColor(0, 0, 0);
+	HAL_Delay(DISCHARGE_DELAY);
+
+	SetColor(0, 1, 0);
+	HAL_Delay(CHARGE_DELAY);
+	result->green = ReadAnalog();
+	SetColor(0, 0, 0);
+	HAL_Delay(DISCHARGE_DELAY);
+
+	SetColor(0, 0, 1);
+	HAL_Delay(CHARGE_DELAY);
+	result->blue = ReadAnalog();
+	SetColor(0, 0, 0);
+
+}
+
+void SetServoAngle(unsigned long channel, uint32_t pulseMicros) {
+	// All three timers are configured to output a 50Hz (20,000 us) square wave,
+	// where the compare value is the high time in us.
+	//
+	// Servos have nominal pulse-width to angle mapping, but they're not perfect.
+	// Better to check specific pulse lengths required to get the settings we need.
+	// Generally, 900-2100us is supposed to represent the range of motion
+	//
+	// Channel is one of three values, each corresponding to a pin:
+	// TIM_CHANNEL_1 = PA_6 = A5
+	// TIM_CHANNEL_2 = PB_5 = D11 AND D5???
+	// TIM_CHANNEL_3 = PB_0 = D3
+	__HAL_TIM_SET_COMPARE(&htim3, channel, pulseMicros);
+}
+
+uint32_t ChutePulseMicros[] = {
+		900,
+		900,
+		900,
+		900,
+		900,
+		900,
+		900,
+		900,
+		900,
+};
+
+void SendChuteTo(int destinationChute) {
+	SetServoAngle(TIM_CHANNEL_1, ChutePulseMicros[destinationChute]);
+}
+
+uint32_t ResetBarClosedMicros = 900;
+uint32_t ResetBarOpenMicros = 2100;
+
+void SetResetBar(int open) {
+	SetServoAngle(TIM_CHANNEL_2, open ? ResetBarOpenMicros : ResetBarClosedMicros);
+}
+
+uint32_t DistributorPulseMicros[] = {
+		900,
+		900,
+		900,
+		900,
+};
+
+void SetDistributorPosition(int position) {
+	SetServoAngle(TIM_CHANNEL_3, DistributorPulseMicros[position]);
+}
+
+unsigned char PrintBuffer[256];
+
+unsigned char rxBuffer[256] = {0};
+// Index of next byte to be read
+unsigned char uartReadIdx = 0;
+// Index of next byte to be written
+unsigned char uartWriteIdx = 0;
+// Whether the write pointer has passed the read pointer
+unsigned char uartOverflow = 0;
+
+unsigned char uartAvailable() {
+	return uartWriteIdx != uartReadIdx;
+}
+
+unsigned char uartRead() {
+	return rxBuffer[uartReadIdx++];
+}
+
+void uartAcknowledgeOverflow() {
+	uartOverflow = 0;
+}
+
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	uartWriteIdx ++; // Will wrap automatically
+	if (uartWriteIdx == uartReadIdx) uartOverflow = 1;
+	HAL_UART_Receive_IT(&huart2, &rxBuffer[uartWriteIdx], 1);
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_ADC1_Init();
+  MX_TIM3_Init();
+  MX_TIM1_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+
+  // Setup UART interrupt
+  HAL_UART_Receive_IT(&huart2, &rxBuffer[uartWriteIdx], 1);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  uint32_t value = 900;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  while (1)
+  {
+
+	  /*
+	  value += 100;
+	  if (value >= 2100) value = 900;
+	  SetServoAngle(TIM_CHANNEL_1, value);
+	  SetServoAngle(TIM_CHANNEL_2, value);
+	  SetServoAngle(TIM_CHANNEL_3, value);
+	  */
+
+	  if (uartAvailable()) uartRead();
+
+	  HAL_UART_Transmit_IT(&huart2, rxBuffer, strlen(rxBuffer));
+	  HAL_Delay(1000);
+
+    /* USER CODE END WHILE */
+
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_TIM1;
+  PeriphClkInit.Tim1ClockSelection = RCC_TIM1CLK_HCLK;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_MultiModeTypeDef multimode = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Common config
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_OVERWRITTEN;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the ADC multi-mode
+  */
+  multimode.Mode = ADC_MODE_INDEPENDENT;
+  if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 8;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 20000-1;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+
+}
+
+/**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 8;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 20000-1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 8;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 20000-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+  HAL_TIM_MspPostInit(&htim3);
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 38400;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA1 PA3 PA4 PA5
+                           PA7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5
+                          |GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ ui/main.py
@@ -0,0 +1,160 @@
+import glob
+import serial
+
+import pygame, sys
+from pygame.locals import *
+
+
+connection = serial.Serial(
+    port = glob.glob('/dev/tty.*')[0], # Mac-only - windows would need COM
+    baudrate = 38400,
+    bytesize = 8,
+    parity = "N",
+    stopbits = 1,
+    timeout=0.001)
+
+grid_color = 120, 120, 120
+background_color = 45, 45, 45
+
+unselected_color = 102, 102, 255
+hover_color = 153, 204, 255
+click_color = 255, 153, 153
+
+text_color = 255, 255, 255
+
+color_table = [
+    (202, 44, 56),
+    (57, 184, 67),
+    (83, 129, 182),
+    (191, 188, 68)]
+
+image_width = 8
+image_height = 8
+
+canvas_scale = 50
+window_width = canvas_scale * image_width
+window_height = canvas_scale * (image_height + 2)
+
+
+mouse_x = 0
+mouse_y = 9
+mouse_click = False
+mouse_down = False
+
+def button(x, y, width, height, text="", color=unselected_color, hover_color=hover_color, click_color=click_color):
+    if x <= mouse_x <= x + width and y <= mouse_y <= y + height:
+        if mouse_down:
+            pygame.draw.rect(window, click_color, (x, y, width, height))
+        else:
+            pygame.draw.rect(window, hover_color, (x, y, width, height))
+
+        draw_text(window, text_color, text, x + width/2, y+height/2, center_x=True, center_y=True)
+        return mouse_click
+    else:
+        pygame.draw.rect(window, color, (x, y, width, height))
+        draw_text(window, text_color, text, x + width/2, y+height/2, center_x=True, center_y=True)
+
+        return False
+    
+def draw_text(surface, color, string, x, y, center_x=False, center_y=False):
+    if string == "": 
+        return
+    text = display_font.render(string, True, color)
+    cx, cy = x, y
+    if center_x:
+        cx -= text.get_width() / 2
+    if center_y:
+        cy -= text.get_height() / 2
+    surface.blit(text, (cx, cy))
+
+canvas = [[0 for _ in range(image_height)] for _ in range(image_width)]
+
+pygame.init()
+pygame.font.init()
+display_font = pygame.font.SysFont("Monaco", 14)
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("MIE438 Candy Sorter")
+clock = pygame.time.Clock()
+
+while True: # main game loop
+
+    mouse_click = False
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_click = True
+            mouse_down = True
+        if event.type == MOUSEBUTTONUP:
+            mouse_down = False
+    
+    # Check mouse state
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+
+    # Clear display
+    pygame.draw.rect(window, background_color, (0, 0, window_width, window_height))
+
+    # Render grid
+    for row in range(image_height+1):
+        pygame.draw.line(window, grid_color, (0, row * canvas_scale), (window_width, row * canvas_scale))
+    
+    for col in range(1, image_width):
+        pygame.draw.line(window, grid_color, (col * canvas_scale, 0), (col * canvas_scale, image_height * canvas_scale))
+
+    for x in range(image_width):
+        for y in range(image_height):
+            canvas[x][y] += 1 if button(x * canvas_scale + 1, 
+                                        y * canvas_scale + 1, 
+                                        canvas_scale - 2, 
+                                        canvas_scale - 2, 
+                                        color=color_table[canvas[x][y]],
+                                        click_color=color_table[(canvas[x][y])],
+                                        hover_color=color_table[canvas[x][y]]) else 0
+            canvas[x][y] %= len(color_table)
+
+    if button(window_width-110, image_height * canvas_scale + 10, 
+              100, 25, "Send"):
+        # Make byte array from image
+        # Each pixel can be two bits
+        # 00 - red
+        # 01 - green
+        # 10 - blue
+        # 11 - yellow
+        # Send an 8x8 image - 16 bytes + one start byte + one stop byte
+        #  01  10  10  01                     (ascii 'i')
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]   (top to bottom, left to right)
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        # [xx][xx][xx][xx] [xx][xx][xx][xx]
+        #  00  11  10  11                     (ascii ';')
+        msg = [ord('i')]
+        for y in range(image_height):
+            for c in range(2):
+                byte = 0
+                for x in range(4):
+                    px = canvas[c*4+x][y]
+                    byte |= px << (6-x*2)
+                msg.append(byte)
+        msg.append(ord(';'))
+        connection.write(bytes(msg))
+
+    if button(window_width-110, image_height * canvas_scale + 40, 
+            100, 25, "Ping"):
+        connection.write(b"p;")
+        
+    pygame.display.update()
+
+    # Render serial data
+    while True:
+        byte = connection.read(1)
+        if len(byte) == 0:
+            break
+        print(byte.decode("utf8"), end="", flush=True)
+        
+    clock.tick(60)
